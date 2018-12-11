bf3f01c48f81b8f9ef7afaa59b723060861b33f3
awitherow <au.witherow@gmail.com>
2018-12-10 17:06:45 -1000

    fix footer link

--- scripts/store.js
+++ scripts/store.js
@@ -84,7 +84,8 @@ function populateVeggieList() {
       description:
         "Also known as Dinosaur Kale, highly nutritious dark green rich tender leaves known for its extraordinary levels of <strong>antioxidants, Vitmain C and K</strong>. Known for its ability to fight cancer, lower cholestoral and reduce heart disease risk.",
       img: "./img/crops/kale.jpg",
-      tags: "smoothies, stir fry, and salads"
+      tags: "smoothies, stir fry, and salads",
+      id: "kale"
     },
     {
       price: "3.00",
@@ -92,7 +93,8 @@ function populateVeggieList() {
       description:
         "Scientifically known as Solanum melongena, the Galine Italian has a rich purple black lustor, contain an impressive array of life sustaining vitmains and minerals, known for promoting gut health, bone and heart health, as well as improving brain function.",
       img: "./img/crops/eggplant.jpg",
-      tags: "stir frys, pastas, roasting, and curries"
+      tags: "stir frys, pastas, roasting, and curries",
+      id: "eggplant"
     },
     {
       price: "3.00",
@@ -100,7 +102,8 @@ function populateVeggieList() {
       description:
         "Rich orange attractive stems and savoyed leaves, Orange Ribbed Swiss Chard is known for its exceptional levels of dietary fiber, Vitamin A, K, Iron and antioxidants while defending against cancer, improving cognitive function and cleansing blood circulation.",
       img: "./img/crops/chard.jpg",
-      tags: "sauteed, salads, bakes and smoothies"
+      tags: "sauteed, salads, bakes and smoothies",
+      id: "chard"
     },
     {
       price: "2.50",
@@ -108,7 +111,8 @@ function populateVeggieList() {
       description:
         "Vibrant green leaves with a mild spice, known for its impressive vitamin and mineral spread, known for its ability to boost the immune system, slow aging, increase metabolism and general overall health from its high Vitamin A content.",
       img: "./img/crops/arugala.jpg",
-      tags: "salads, pastas, and pizzas"
+      tags: "salads, pastas, and pizzas",
+      id: "arugala"
     },
     {
       price: "3.50",
@@ -116,7 +120,8 @@ function populateVeggieList() {
       description:
         "Strikingly beautiful colors, with excellent sweet flavour, boasting a variety of vitamins and minerals that work in combination to prevent heart disease and cancer, promote DNA healing, as well as regulate heart rate and metabolism.",
       img: "./img/crops/beets.jpg",
-      tags: "salads, juicing, and smoothies"
+      tags: "salads, juicing, and smoothies",
+      id: "beets"
     },
     {
       price: "2.50",
@@ -124,7 +129,8 @@ function populateVeggieList() {
       description:
         "These beautiful, dark, glossy oval shaped leaves are high in Vitamins A, C, K1, B-6, B-9, folic acid, iron, calcium and more. These empower you to slow aging, reduce the risk of cancer, improve eye health, regulate blood pressure and promote heart health.",
       img: "./img/crops/spinach.jpg",
-      tags: "pizza, omelettes, smoothies, juices, salads and more"
+      tags: "pizza, omelettes, smoothies, juices, salads and more",
+      id: "spinach"
     }
   ];
 
@@ -135,7 +141,8 @@ function populateVeggieList() {
       description:
         "These dark green smooth leaves rich in phyto-nutrients that combat various types of cancer, natural anti-bacterial and anti-virual properties, packed with Vitamin C, A, K, B-3, B-5, and riboflavin, and boast a an impressive essential minerals profile.",
       img: "./img/crops/collard.jpg",
-      tags: "steamed, sauteed, and stews"
+      tags: "steamed, sauteed, and stews",
+      id: "collards"
     },
     {
       price: "2.50",
@@ -143,7 +150,8 @@ function populateVeggieList() {
       description:
         "These thick flavorful leaves boast impressive health benefits, including being an anti-inflammitory agent, protector of neuronal cells, cholestoral fighter, sleep aid, cancer fighter, anxiety controller and anti-microbial defenses.",
       img: "./img/crops/lettuce.jpg",
-      tags: "sandwiches, salads and juices"
+      tags: "sandwiches, salads and juices",
+      id: "lettuce"
     },
     {
       price: "3.50",
@@ -151,7 +159,8 @@ function populateVeggieList() {
       description:
         "This beautiful multicolor mix are crisp and mild, packed with Vitamins A, C, potassium, zinc and dietary fiber. They also contain enzymes such as myrosinase, diastase, esterases, and amylase which have anti-fungal properties and are known to promote digestive health.",
       img: "./img/crops/radish.jpg",
-      tags: "salads, stews, roasts or even pickled."
+      tags: "salads, stews, roasts or even pickled.",
+      id: "radish"
     },
     {
       price: "1.50",
@@ -159,7 +168,8 @@ function populateVeggieList() {
       description:
         "These delicious fruits native to Hawaii are packed with an extremely impressive array of vitamins and minerals critical for a healthy existence. Boosting immunity, preventing cancer, aiding digestion, reducing blood pressure and more, Lilikoi are the perfect treat.",
       img: "./img/crops/lilikoi.jpg",
-      tags: "desserts, punches, juices, smoothies and drinks"
+      tags: "desserts, punches, juices, smoothies and drinks",
+      id: ""
     },
     {
       price: "5.00",
@@ -167,7 +177,8 @@ function populateVeggieList() {
       description:
         "A delicious gift to the world, the Kamiya Laie Gold Papaya will leave you glowing with its high dietary fiber content, immune boosting and anti-inflammitory agents, anti aging and skin care nutrients, as well as overall bodily fluid hardiness and health. The seeds are also endowed with magical properties.",
       img: "./img/crops/papaya.jpg",
-      tags: "desserts, salads, juices and smoothies."
+      tags: "desserts, salads, juices and smoothies.",
+      id: ""
     },
     {
       price: "0.00",
@@ -175,7 +186,8 @@ function populateVeggieList() {
       description:
         "North Shore Agricultural Lands of Oahu are especially fertile. We are interested in cultivating fast growing crops that thrive in Hardiness Zone 12B, or transplantable fruit trees 1-3 years in age. Let us know if you have ideas!",
       img: "./img/crops/dream.jpg",
-      tags: "whatever you can imagine!!"
+      tags: "whatever you can imagine!!",
+      id: ""
     }
   ];
 
