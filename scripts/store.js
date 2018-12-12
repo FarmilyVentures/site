1e093dbee37bbce02d13080a4e098cbf07035389
awitherow <au.witherow@gmail.com>
2018-12-11 16:36:31 -1000

    prepare crop selector:

--- scripts/store.js
+++ scripts/store.js
@@ -1,9 +1,32 @@
+// TOC
+// 1. startup - self invoking
+// 2. setEventListeners - handles all event listeners
+// 3. handleOrder - order signup form handler
+// 4. populateVeggieList - render veggie cards
+// 5. handleQuantityChange - updates localstorage store and UI of deisred order.
+// 6. renderShoppingCartItems - updates order form quantities of veggies above form
+
 (function startup() {
+  populateVeggieList();
+  setEventListeners();
+})();
+
+function setEventListeners() {
+  // order form
   const form = document.getElementById("order-form");
   form.addEventListener("submit", handleOrder);
 
-  populateVeggieList();
-})();
+  for (let selector of document.getElementsByClassName("crop-selector")) {
+    var priorQuantity = getFromLocalStorage(selector.name);
+
+    if (priorQuantity) {
+      document.getElementById(
+        "selector-" + selector.name
+      ).value = priorQuantity;
+    }
+    selector.addEventListener("change", handleQuantityChange);
+  }
+}
 
 // email form for consulting handling
 function handleOrder(e) {
@@ -83,7 +106,8 @@ function populateVeggieList() {
         "Also known as Dinosaur Kale, highly nutritious dark green rich tender leaves known for its extraordinary levels of <strong>antioxidants, Vitmain C and K</strong>. Known for its ability to fight cancer, lower cholestoral and reduce heart disease risk.",
       img: "./img/crops/kale.jpg",
       tags: ["smoothies", "stirfrys", "salads"],
-      id: "kale"
+      id: "kale",
+      stocked: true
     },
     {
       price: "3.00",
@@ -92,7 +116,8 @@ function populateVeggieList() {
         "Scientifically known as Solanum melongena, the Galine Italian has a rich purple black lustor, contain an impressive array of life sustaining vitmains and minerals, known for promoting gut health, bone and heart health, as well as improving brain function.",
       img: "./img/crops/eggplant.jpg",
       tags: ["stirfrys", "pastas", "roasts", "curries"],
-      id: "eggplant"
+      id: "eggplant",
+      stocked: true
     },
     {
       price: "3.00",
@@ -101,7 +126,8 @@ function populateVeggieList() {
         "Rich orange attractive stems and savoyed leaves, Orange Ribbed Swiss Chard is known for its exceptional levels of dietary fiber, Vitamin A, K, Iron and antioxidants while defending against cancer, improving cognitive function and cleansing blood circulation.",
       img: "./img/crops/chard.jpg",
       tags: ["sauteed", "salads", "bakes", "smoothies"],
-      id: "chard"
+      id: "chard",
+      stocked: true
     },
     {
       price: "2.50",
@@ -110,7 +136,8 @@ function populateVeggieList() {
         "Vibrant green leaves with a mild spice, known for its impressive vitamin and mineral spread, known for its ability to boost the immune system, slow aging, increase metabolism and general overall health from its high Vitamin A content.",
       img: "./img/crops/arugala.jpg",
       tags: ["salads", "pastas", "pizzas"],
-      id: "arugala"
+      id: "arugala",
+      stocked: true
     },
     {
       price: "3.50",
@@ -119,7 +146,8 @@ function populateVeggieList() {
         "Strikingly beautiful colors, with excellent sweet flavour, boasting a variety of vitamins and minerals that work in combination to prevent heart disease and cancer, promote DNA healing, as well as regulate heart rate and metabolism.",
       img: "./img/crops/beets.jpg",
       tags: ["salads", "juices", "smoothies"],
-      id: "beets"
+      id: "beets",
+      stocked: true
     },
     {
       price: "2.50",
@@ -128,11 +156,9 @@ function populateVeggieList() {
         "These beautiful, dark, glossy oval shaped leaves are high in Vitamins A, C, K1, B-6, B-9, folic acid, iron, calcium and more. These empower you to slow aging, reduce the risk of cancer, improve eye health, regulate blood pressure and promote heart health.",
       img: "./img/crops/spinach.jpg",
       tags: ["pizzas", "omelettes", "smoothies", "juices", "salads"],
-      id: "spinach"
-    }
-  ];
-
-  var futureRounds = [
+      id: "spinach",
+      stocked: true
+    },
     {
       price: "3.00",
       title: "Flash Collard Greens",
@@ -140,16 +166,8 @@ function populateVeggieList() {
         "These dark green smooth leaves rich in phyto-nutrients that combat various types of cancer, natural anti-bacterial and anti-virual properties, packed with Vitamin C, A, K, B-3, B-5, and riboflavin, and boast a an impressive essential minerals profile.",
       img: "./img/crops/collard.jpg",
       tags: ["steamed", "sauteed", "stews"],
-      id: "collards"
-    },
-    {
-      price: "2.50",
-      title: "Sylvestia Lettuce",
-      description:
-        "These thick flavorful leaves boast impressive health benefits, including being an anti-inflammitory agent, protector of neuronal cells, cholestoral fighter, sleep aid, cancer fighter, anxiety controller and anti-microbial defenses.",
-      img: "./img/crops/lettuce.jpg",
-      tags: ["sandwiches", "salads", "juices"],
-      id: "lettuce"
+      id: "collards",
+      stocked: true
     },
     {
       price: "3.50",
@@ -158,8 +176,22 @@ function populateVeggieList() {
         "This beautiful multicolor mix are crisp and mild, packed with Vitamins A, C, potassium, zinc and dietary fiber. They also contain enzymes such as myrosinase, diastase, esterases, and amylase which have anti-fungal properties and are known to promote digestive health.",
       img: "./img/crops/radish.jpg",
       tags: ["salads", "stews", "roasts", "pickled"],
-      id: "radish"
+      id: "radish",
+      stocked: true
     },
+    {
+      price: "2.50",
+      title: "Sylvestia Lettuce",
+      description:
+        "These thick flavorful leaves boast impressive health benefits, including being an anti-inflammitory agent, protector of neuronal cells, cholestoral fighter, sleep aid, cancer fighter, anxiety controller and anti-microbial defenses.",
+      img: "./img/crops/lettuce.jpg",
+      tags: ["sandwiches", "salads", "juices"],
+      id: "lettuce",
+      stocked: true
+    }
+  ];
+
+  var futureRounds = [
     {
       price: "1.50",
       title: "Hawaiian Lilikoi (Passionfruit)",
@@ -167,7 +199,8 @@ function populateVeggieList() {
         "These delicious fruits native to Hawaii are packed with an extremely impressive array of vitamins and minerals critical for a healthy existence. Boosting immunity, preventing cancer, aiding digestion, reducing blood pressure and more, Lilikoi are the perfect treat.",
       img: "./img/crops/lilikoi.jpg",
       tags: ["desserts", "punches", "juices", "smoothies", "drinks"],
-      id: ""
+      id: "",
+      stocked: false
     },
     {
       price: "5.00",
@@ -176,7 +209,8 @@ function populateVeggieList() {
         "A delicious gift to the world, the Kamiya Laie Gold Papaya will leave you glowing with its high dietary fiber content, immune boosting and anti-inflammitory agents, anti aging and skin care nutrients, as well as overall bodily fluid hardiness and health. The seeds are also endowed with magical properties.",
       img: "./img/crops/papaya.jpg",
       tags: ["desserts", "salads", "juices", "smoothies"],
-      id: ""
+      id: "",
+      stocked: false
     },
     {
       price: "0.00",
@@ -185,7 +219,8 @@ function populateVeggieList() {
         "North Shore Agricultural Lands of Oahu are especially fertile. We are interested in cultivating fast growing crops that thrive in Hardiness Zone 12B, or transplantable fruit trees 1-3 years in age. Let us know if you have ideas!",
       img: "./img/crops/dream.jpg",
       tags: ["anything"],
-      id: ""
+      id: "",
+      stocked: false
     }
   ];
 
@@ -211,12 +246,27 @@ function populateVeggieList() {
 
       tags = tags.replace(/,/g, "");
 
-      var cropQuantitySelector = crop.id.length
-        ? "<div class='button-container'><button class='toggle'><i class='fas fa-caret-down'></i></button><p class='crop-count'>0</p><button class='toggle'><i class='fas fa-caret-up'></i></button></div>"
-        : null;
+      var numberList = Array.from(Array(51), function(_, x) {
+        return x;
+      });
+
+      var quantity =
+        crop.stocked &&
+        "<select id='selector-" +
+          crop.id +
+          "' class='crop-selector' name=" +
+          crop.id +
+          ">" +
+          numberList.map(function(x) {
+            return "<option value=" + x + ">" + x + "</option>";
+          }) +
+          "</select>";
 
       container.innerHTML +=
-        "<div class='card'><div class='price-tag'>$" +
+        "<div class='card'><div class='price-tag'>" +
+        quantity +
+        "x" +
+        " $" +
         crop["price"] +
         "<small>/pound</small></div><img src='" +
         crop["img"] +
@@ -224,8 +274,17 @@ function populateVeggieList() {
         crop["title"] +
         "</h5><p>" +
         crop["description"] +
-        tags + cropQuantitySelector
+        tags +
         "</div></div>";
     });
   });
+}
+
+function handleQuantityChange() {
+  setToLocalStorage(this.name, this.value);
+  renderShoppingCartItems();
+}
+
+function renderShoppingCartItems() {
+  var shoppingCart = document.getElementById("shopping-cart");
 }
